58186329f5deb9e09fe1d02d3a8f31baa4725672
Alasdair Blackwell <aliblackwell@gmail.com>
2018-02-03 15:48:16 +0000

    Wired up to Ether Classic wallet balance

--- balance-checker/index.js
+++ balance-checker/index.js
@@ -9,19 +9,20 @@ require('dotenv').config();
 
 var classicContractAddress = process.env.CLASSIC_CONTRACT;
 var classicWalletAddress = process.env.CLASSIC_WALLET;
+var classicTokenAddress = process.env.CLASSIC_TOKEN;
 var ethereumContractAddress = process.env.ETHER_CONTRACT;
 var ethereumWalletAddress = process.env.ETHER_WALLET;
 var etherscanAPIKey = process.env.ETHERSCAN_KEY;
 
 var updateJson = new Promise( (resolve, reject) => {
     var data = {
-        "ETC_contract": null,
+        "ETC_wallet": null,
         "NCC_tokens_left": null,
         "NCCh_tokens_left": null
     };
 
     var nccTokens;
-    var classicTokensInAccount = new Promise((resolve, reject)=> { request('https://gastracker.io/token/'+classicContractAddress+'/'+classicContractAddress, function (error, response, body) {
+    var classicTokensInAccount = new Promise((resolve, reject)=> { request('https://gastracker.io/token/'+classicTokenAddress+'/'+classicTokenAddress, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             const dom = new JSDOM(body);
             const untrimmedContent = dom.window.document.querySelectorAll("dd")[2].textContent;
@@ -39,17 +40,17 @@ var updateJson = new Promise( (resolve, reject) => {
     });
 
     var etherClassic;
-    var etherInContract = new Promise((resolve, reject)=> { request('http://gastracker.io/addr/'+classicWalletAddress, function (error, response, body2) {
+    var etherClassicInWallet = new Promise((resolve, reject)=> { request('http://gastracker.io/addr/'+classicWalletAddress, function (error, response, body2) {
         if (!error && response.statusCode == 200) {
         const dom2 = new JSDOM(body2);
         const untrimmedContent2 = dom2.window.document.querySelectorAll("dd")[2].textContent;
         const trimedContent2 = untrimmedContent2.replace(/ /g,'');
         const number2 = trimedContent2.replace(/Ether/, ''); // bad because we may have to change? Nahh
         //   const number2 = trimedContent2.match(/(\d+)/); // would be better but is not working
-        var etherClassic = parseInt(number2);
-        console.log('Contract Ether: ' + etherClassic);
-        data["ETC_contract"] = etherClassic;
-        resolve(etherClassic);
+        //var etherClassic = parseInt(number2);
+        console.log('Classic Wallet Ether: ' + number2);
+        data["ETC_wallet"] = number2;
+        resolve(number2);
     } else{
             console.log(error);
         }
@@ -79,7 +80,7 @@ var updateJson = new Promise( (resolve, reject) => {
     });
 
     var data = {
-        "ETC_contract": etherClassic,
+        "ETC_wallet": etherClassic,
         "NCC_tokens_left": nccTokens,
         "NCCh_tokens_left": ncchTokens
     };
@@ -89,10 +90,10 @@ var updateJson = new Promise( (resolve, reject) => {
     // NOTE: is going to be very hard to do number of people
     // need the below file to be written in a promise
 
-    // when(data["ETC_contract"] != null && data["NCC_tokens_left"] != null && data["NCCh_tokens_left"] != null).then(function(){
-    // Promise.all([ethereumData , etherInContract , classicTokensInAccount], function(values){
+    // when(data["ETC_wallet"] != null && data["NCC_tokens_left"] != null && data["NCCh_tokens_left"] != null).then(function(){
+    // Promise.all([ethereumData , etherClassicInWallet , classicTokensInAccount], function(values){
 
-    Promise.all([ethereumData, etherInContract, classicTokensInAccount]).then(function(value){ //this works but is not ideal
+    Promise.all([ethereumData, etherClassicInWallet, classicTokensInAccount]).then(function(value){ //this works but is not ideal
         console.log("we're in ");
         data["timeStamp"] = moment().unix();
         console.log(data["timeStamp"]);
